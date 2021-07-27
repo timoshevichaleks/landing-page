7b1fb0e927ed2134fbbc81e152c1d42f3c6ef815
timoshevichaleks <alti2692@gmail.com>
2021-07-27 17:11:00 +0300

    сделал навигацию по странице

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,42 @@
-(function($) {})(jQuery);
+(function($) {
+  $('#back2top').on('click', function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 500);
+  });
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 100) {
+
+      $('#header').addClass('header-scroll');
+      $('#navbar').addClass('navbar-scroll');
+    } else {
+      $('#header').removeClass('header-scroll');
+      $('#navbar').removeClass('navbar-scroll');
+    }
+  });
+
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > 600) {
+
+      $('#back2top').show();
+    } else {
+      $('#back2top').hide();
+    }
+  });
+
+  $('#navbar').scroll(function() {
+    if ($(this).scrollTop() < 100) {
+      $('nav li').removeClass('active-link');
+    }
+  })
+
+  $('#navbar').onePageNav({
+    currentClass: 'active-link',
+    scrollThreshold: 0.3,
+    scrollSpeed: 750,
+    easing: 'swing',
+    filter: ':not(.navbar-brand)'
+  });
+
+})(jQuery);
